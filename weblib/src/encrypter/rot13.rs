2c9bd655afcf914b8937f9e97544e759be5c639a
b4rdarian <con.anasta@gmail.com>
2023-12-26 11:38:56 +0100

    Even simpler with match

--- weblib/src/encrypter/rot13.rs
+++ weblib/src/encrypter/rot13.rs
@@ -2,15 +2,13 @@ pub struct Rot13(pub String);
 
 impl super::Encryptable for Rot13 {
     fn encrypt(&self) -> String {
-        self.0.chars().map(|ch| {
-            if (ch >= 'a' && ch < 'n') || ( ch >= 'A' &&  ch < 'N') {
-                (ch as u8 + 13) as char
-            } else if (ch >= 'n' && ch < 'z') || ( ch >= 'N' &&  ch < 'Z') {
-                (ch as u8 - 13) as char
-            } else {
-                ch
-            }
-
-        }).collect()
+        self.0
+            .chars()
+            .map(|ch| match ch {
+                'a'..='m' | 'A'..='M' => (ch as u8 + 13) as char,
+                'n'..='z' | 'N'..='Z' => (ch as u8 - 13) as char,
+                _ => ch,
+            })
+            .collect()
     }
 }
